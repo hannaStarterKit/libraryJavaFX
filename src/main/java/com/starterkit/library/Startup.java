e7a292db77156d9b988928c36b873e95d4cafe4a
hannaStarterKit <hnsienkiewicz@gmail.com>
2016-06-16 11:08:09 +0200

    first commit

---
+++ src/main/java/com/starterkit/library/Startup.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.starterkit.library;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author HSIENKIE
+ *
+ */
+public class Startup extends Application {
+
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
